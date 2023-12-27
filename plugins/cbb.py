051948c7aa5cda491cf47418378b3d0307007ab2
crazydeveloper3 <139277159+crazydeveloper3@users.noreply.github.com>
2023-12-27 14:09:41 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/professorr_x'>Contact me</a>\n○ Movie Group : <a href='https://t.me/terabox_movie_request_group'\n\n Thanks you @Professorr_x for making this bot. </b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/professorr_x'>Contact me</a>\n○ Movie Group : <a href='https://t.me/terabox_movie_request_group'>JOIN NOW </a> \n\n Thanks you @Professorr_x for making this bot. </b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
